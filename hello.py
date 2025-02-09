d0d316cc93b4226e87524a44afd6669c1c89da2d
Jocel Encio <jocelencio25@gmail.com>
2025-02-09 20:52:05 +0800

    My First Python Program

---
+++ hello.py
@@ -0,0 +1,9 @@
+Name = "Jocel Encio"
+Course = "BSIT"
+Interests_in_programming = "Python"
+
+print("Hello world!")
+
+print("Name: " + Name)
+print("Course: " + Course)
+print("Interests in programming: " + Interests_in_programming)
